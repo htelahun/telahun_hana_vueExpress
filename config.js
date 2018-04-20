bdf4a862b1efe63d159f5f63896e612de193d45f
Hana Telahun <hanatelahun@Hanas-MacBook-Pro-2.local>
2018-04-20 03:03:13 -0400

    first draft

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+
+host : "localhost",
+
+user : "root",
+
+pass : "root",
+
+port : 8889,
+
+database : "db_roku"
+
+   };
+
+module.exports = config ;
---
+++ public/javascripts/main.js
@@ -0,0 +1,25 @@
+var myApp ={
+  movieGenres(data, genres){
+    //filter the dataset and create an array of genres -> one object fr each genre in the database
+    genres.forEach((genre, index)=> myApp.vm.genres.push({name: genre, movies:data.filter(movie => movie.genre_name === genre) }));
+  },
+
+  vm : new Vue({
+
+    delimiters : ["${","}"],
+    el : "#app",
+
+    data : {
+      message : "Roku video player",
+      genres: []
+    },
+
+    methods :{
+
+    }
+
+
+  })
+}
+
+myApp.movieGenres(appData.movies, ["ACTION", "COMEDY", "DRAMA"]);
