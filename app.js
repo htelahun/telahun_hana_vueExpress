24beaefd519a5a77f1f1d28eba62c66f1d4e15b7
Hana Telahun <hanatelahun@Hanas-MacBook-Pro-2.local>
2018-04-20 20:53:12 -0400

    kids progress

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/kidsMovies', require('./routes/kidsMovies'));
 app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
--- routes/kidsMovies.js
+++ routes/kidsMovies.js
@@ -4,44 +4,41 @@ var connect = require('../utils/sqlConnect');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  connect.query(`SELECT * FROM tbl_movies m, tbl_genre g, tbl_mov_gen mg WHERE m.movie_id = mg.movie_id AND g.genre_id = mg.genre_id`, (error, rows)=>{
+  connect.query(`SELECT * FROM tbl_kids k, tbl_genre g, tbl_mov_gen mg WHERE k.kids_id = mg.movie_id AND g.genre_id = mg.genre_id`, (error, rows)=>{
     if (error){
       throw error;
     }else{
-      res.render('home',
-            {title: 'Rendel',
-            desc: "Rendel is a 2017 Finnish superhero film written, produced and directed by Jesse Haaja. The film is based on Haaja's self-created superhero, Rendel. Lead roles are played by Kris Gummerus, Matti Onnismaa, Rami Rusinen and Renne Korppila. The rest of the cast includes Alina Tomnikov, Tero Salenius, Aake Kalliala, Anu Palevaara and Kristina Karjalainen.",
+      res.render('kids',
+            { title: "The Boss Baby",
+       desc : "A man named Tim Templeton tells a story about his 7-year-old self who lives with his parents, Ted and Janice. Ted and Janice work in a puppy factory called Puppy Co. founded by a man named Francis E. Francis. ",
 
             //defaultMovie : rows[Math.floor(Math.random()*rows.length)],
             data: JSON.stringify(rows),
-            index:true,
-            mainpage: true,
+            mainpage: false,
             videopage: false,
             kidsplayer: false,
-            kidspage: false
-
+            kidspage: true
        });
     }
   })
 
   });
 
-  router.get('/movies/:id/:vidsrc', (req,res)=> {
+  router.get('/kids-movies/:id/:vidsrc', (req,res)=> {
 // get the movie details from the server
 console.log(req.params.id, req.params.vidsrc);
 connect.query(`SELECT * FROM tbl_comments WHERE comments_movie = "${req.params.id}"`, (err,rows)=>{
   if(err){
     console.log(err);
   }else {
-    res.render('movie',{
+    res.render('kidsPlayer',{
       movie:req.params.id,
       trailer: req.params.vidsrc,
       data: JSON.stringify(rows),
       mainpage: false,
-      videopage: true,
-      kidsplayer: false,
+      videopage: false,
+      kidsplayer: true,
       kidspage: false
-
         });
       }
     })
